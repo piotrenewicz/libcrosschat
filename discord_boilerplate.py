4370d15ea4314b72932b359b5f1f814daac37ffd
Piotrenewicz <piotrekm9@onet.eu>
2021-03-20 12:40:08 +0100

    discord_backend updated with Mykhailo recommendations

--- discord_boilerplate.py
+++ discord_boilerplate.py
@@ -1,19 +1,18 @@
 import discord
+import tokens.discord_config
 
-token = "ODE5MjU3MDU4ODQzMDMzNjEx.YEj-lw.ZaxAr0iPPXwMqlJUkqv-lswW-ik"
+if tokens.discord_config.enable:
+    client = discord.Client()
 
-client = discord.Client()
+    @client.event
+    async def on_message(message: discord.message.Message):
+        print(message.content)
+        print(type(message))
+        if message.author == client.user: return  # ignore self ## this fixes looping on response to self
+        if message.author.bot: return  # ignore all bots ## this also fixes two bots replying to each other
+        await message.channel.send("responding to message " + message.content)
 
+    if __name__ == "__main__":
+        pass  # in this version there is no main code for the bot to serve, testing response will be moved here later.
 
-@client.event
-async def on_message(message: discord.message.Message):
-    print(message.content)
-    print(type(message))
-    # FIXME looping on response to self
-    if message.author == client.user: return  # ignore self
-    if message.author.bot: return  # ignore all bots
-    await message.channel.send("responding to message " + message.content)
-
-
-# run after
-client.run(token)
+    client.run(tokens.discord_config.TOKEN)  # this is probably blocking, will require threading.
---
+++ tokens/discord_config.py
@@ -0,0 +1,4 @@
+# To enable discord backend; put your token into the TOKEN variable, and set enable boolean to true.
+
+enable = False
+TOKEN = "While building a bot put your bot token here. Remember to don't share or send this file to github, after filling in the token"
