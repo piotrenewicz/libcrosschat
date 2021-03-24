6ac4fdfa92dcf9f60adcb2e330b6d23fc82b7677
Mykhailo Marchenko <mehanikm@gmail.com>
2021-03-24 19:18:28 +0100

    Refactor config files

--- tokens/discord_config.py
+++ tokens/discord_config.py
@@ -1,4 +1,4 @@
-# To enable discord backend; put your token into the TOKEN variable, and set enable boolean to true.
+# To enable Discord backend; put your token into the DISCORD_TOKEN variable, and set DISCORD_ENABLE boolean to true.
 
-enable = False
-TOKEN = "While building a bot put your bot token here. Remember to don't share or send this file to github, after filling in the token"
+DISCORD_ENABLE = False
+DISCORD_TOKEN = "While_building_a_bot_put_your_bot_token_here__Remember_not_to_share_or_send_this_file_to_github_after_filling_in_the_token"
