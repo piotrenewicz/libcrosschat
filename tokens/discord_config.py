4b2968ce1717151adb2b9271a17591ade9f1bd21
Piotr Morel <35681417+piotrenewicz@users.noreply.github.com>
2021-03-24 20:05:36 +0100

    fix

--- tokens/discord_config.py
+++ tokens/discord_config.py
@@ -1,4 +1,4 @@
 # To enable Discord backend; put your token into the DISCORD_TOKEN variable, and set DISCORD_ENABLE boolean to true.
 
-enable = True
-TOKEN = "ODE5MjU3MDU4ODQzMgit DMzNjEx.YEj-lw.bMSLXRfMTWLZN1ijCyFAN_rJ7wQ"
+enable = False
+TOKEN = "While building a bot put your bot token here. Remember to don't share or send this file to github, after filling in the token"
