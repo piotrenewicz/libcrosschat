66e6ab3280487b17623ebe97dfffee30ead47235
Piotrenewicz <piotrekm9@onet.eu>
2021-03-29 03:21:34 +0200

    A lot of changes 1. distributed enable flag to every backend. (it's needed for compatibility with main) 2. Importing backend config files is now resistant to lack of file. (this needs extra work, check enable, before you access your token!) 3. Adding new backends is now easier than ever!, you can just append to a list of backends to be served! 4. Implemented Threading into main code. All backends can run in parallel. 5. DiscordBackend can't run in a non-main Thread. Fixed! (learned a lot about asynchronous coroutines, proper implementation in next commit) 6. Separated registering a user_function from initializing the backends, registering user_function is now called attaching. And is handled with a decorator! This can be extended to provide attachments for things other than text 7. Because of the separation (6), user_code now needs to run crosschatbotAPI.begin_backends() at the end. To initialize the backends and begin serving the bot. user_code also needs park the main Thread, or else the program ends. This is improved in next commit

--- tokens_template/facebook_config.py
+++ tokens_template/facebook_config.py
@@ -1,5 +1,5 @@
 # To enable Facebook backend; put your token into the FACEBOOK_TOKEN variable, and set FACEBOOK_ENABLE boolean to true.
 
-FACEBOOK_ENABLE = False
+enable = False
 ACCESS_TOKEN = "While_building_a_bot_put_your_bot_token_here__Remember_not_to_share_or_send_this_file_to_github_after_filling_in_the_token"
 VERIFY_TOKEN = "While_building_a_bot_put_your_bot_token_here__Remember_not_to_share_or_send_this_file_to_github_after_filling_in_the_token"
