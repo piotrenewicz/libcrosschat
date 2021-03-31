bbcd35b385290064aa2710187f278cbeb47fdce9
Piotrenewicz <piotrekm9@onet.eu>
2021-03-31 23:12:46 +0200

    if enable: implemented

--- TelegramBackend.py
+++ TelegramBackend.py
@@ -6,8 +6,9 @@ try:
 except ModuleNotFoundError:
     enable = False
 
-# Initialize bot with given token
-bot = telebot.TeleBot(TOKEN)
+if enable:
+    # Initialize bot with given token
+    bot = telebot.TeleBot(TOKEN)
 
 
 def run():
@@ -26,15 +27,16 @@ def register_text_endpoint(_message_function):
     text_function = _message_function
 
 
-# Handle simple text messages
-@bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location', 'contact', 'new_chat_members', 'left_chat_member', 'new_chat_title',
-                     'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message'])
-def respond(message: telebot.types.Message):
-    if text_function is unconfigured:
-        return
-    bot.send_chat_action(message.chat.id, 'typing')
-    bot.send_message(message.chat.id, text_function(
-        message.text, message.chat.first_name))
+if enable:
+    # Handle simple text messages
+    @bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location', 'contact', 'new_chat_members', 'left_chat_member', 'new_chat_title',
+                         'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message'])
+    def respond(message: telebot.types.Message):
+        if text_function is unconfigured:
+            return
+        bot.send_chat_action(message.chat.id, 'typing')
+        bot.send_message(message.chat.id, text_function(
+            message.text, message.chat.first_name))
 
 
 # Start bot's polling
