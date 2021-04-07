8027883b99820fec9bb2ca86ceefb835a3b352dc
Piotrenewicz <piotrekm9@onet.eu>
2021-04-07 22:20:09 +0200

    full function support from telegram

--- TelegramBackend.py
+++ TelegramBackend.py
@@ -20,11 +20,16 @@ def unconfigured(*args, **kwargs):
 
 
 text_function = unconfigured
-
+full_function = unconfigured
 
 def register_text_endpoint(_message_function):
     global text_function
     text_function = _message_function
+
+
+def register_full_endpoint(_full_function):
+    global full_function
+    full_function = _full_function
 
 
 if enable:
@@ -32,11 +37,15 @@ if enable:
     @bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location', 'contact', 'new_chat_members', 'left_chat_member', 'new_chat_title',
                          'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message'])
     def respond(message: telebot.types.Message):
-        if text_function is unconfigured:
-            return
-        bot.send_chat_action(message.chat.id, 'typing')
-        bot.send_message(message.chat.id, text_function(
-            message.text, message.chat.first_name))
+        if text_function is not unconfigured:
+            bot.send_chat_action(message.chat.id, 'typing')
+            bot.send_message(message.chat.id, text_function(
+                message.text, message.chat.first_name))
+
+        if full_function is not unconfigured:
+            def responder(response):
+                bot.send_message(message.chat.id, response)
+            full_function(("TG", message.chat.id), message.text, message.chat.first_name+" "+message.chat.last_name, responder)
 
 
 # Start bot's polling
