aeb21338d95ad9e2c066a9611b3a1c6658ad6133
Piotrenewicz <piotrekm9@onet.eu>
2021-04-07 20:36:45 +0200

    Split simple demo from full crosschat demo

--- user_code_crosschat_demo.py
+++ user_code_crosschat_demo.py
@@ -1,11 +1,4 @@
 import crosschatbotAPI
-import time
-
-# @crosschatbotAPI.attach_text
-# def this_is_client_job(message: str, author: str):
-#     time.sleep(4)
-#     return "Hello, you've contacted client code function!\nUnfortunately we are still under construction.\nLong story short, you're not getting any other response from us today!" + message + author
-
 
 crosschats = dict()
 rooms = dict()
