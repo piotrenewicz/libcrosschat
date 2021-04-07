9fd38f5c4da410a1d64009f60026204935106c82
Piotrenewicz <piotrekm9@onet.eu>
2021-04-07 22:39:48 +0200

    telegram token config fixed

--- tokens_template/telegram_config.py
+++ tokens_template/telegram_config.py
@@ -1,4 +1,4 @@
 # To enable Telegram backend; put your token into the TELEGRAM_TOKEN variable, and set TELEGRAM_ENABLE boolean to true.
 
 enable = False
-TELEGRAM_TOKEN = "While_building_a_bot_put_your_bot_token_here__Remember_not_to_share_or_send_this_file_to_github_after_filling_in_the_token"
+TOKEN = "While_building_a_bot_put_your_bot_token_here__Remember_not_to_share_or_send_this_file_to_github_after_filling_in_the_token"
