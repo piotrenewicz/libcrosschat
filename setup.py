c14e1553b69b200cf6e27879b8ed48c2693b4864
Piotrenewicz <piotrekm9@onet.eu>
2021-04-15 00:03:52 +0200

    setup.py attempt

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name='libcrosschat',
+    packages=['libcrosschat'],
+    version='1.0.0',
+    install_requires=requirements,
+    description="Python module to make chatbots for multiple Platforms",
+    author='Piotr Morel, Konrad Maciejczyk, Mykhailo Marchenko',
+    author_email='piotr.morel@smcebi.edu.pl',
+    url='https://github.com/piotrenewicz/libcrosschatrepo',
+    keywords=['chatbot', 'chatbots', 'chat-bot', 'chatbot-framework', 'discord', 'telegram', 'facebook messenger', 'python', 'bot'],
+    classifiers=[],
+)
